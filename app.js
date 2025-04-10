15f6c5aed98c64cb59e6836c2180a86c9da99896
YosiMar13 <madranoe4@gmail.com>
2025-04-10 11:26:08 -0500

    primer commit

---
+++ app.js
@@ -0,0 +1,9 @@
+
+const showMessage=(message)=>{
+    return message
+}
+    console.log(showMessage('Hola cabeza de bola' ))
+    var a=7;
+    var b=10;
+    var r=a+b;
+    console.log(`el resulado es: ${r}`)
